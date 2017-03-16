66228087ed6a8febeb423960896e06e84a7024e7
Matt Masuda <matt.masuda@aa.com>
2017-03-16 15:48:53 -0500

    Add items and ngFor

--- src/app/release-list/release-list.component.ts
+++ src/app/release-list/release-list.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReleaseListComponent implements OnInit {
 
+  releases: any[] = [
+    {
+      "id": 1,
+      "customer": "FH",
+      "description": "IMH jar update"
+    },
+    {
+      "id": 2,
+      "customer": "Airops",
+      "description": "FOMPS release"
+    },
+    {
+      "id": 3,
+      "customer": "LKA",
+      "description": "RAFT change"
+    }
+
+  ]
   constructor() { }
 
   ngOnInit() {
