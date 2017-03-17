ffd1e480970dcc207e82924ddab2b5f9a97be4cd
Matt Masuda <matt.masuda@aa.com>
2017-03-17 09:47:54 -0500

    Set up data binding for list items

--- src/app/release-item/release-item.component.ts
+++ src/app/release-item/release-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fh-release-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReleaseItemComponent implements OnInit {
 
+  @Input() item : any;
   constructor() { }
 
   ngOnInit() {
